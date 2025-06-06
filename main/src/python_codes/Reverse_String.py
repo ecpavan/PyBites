461b8b826cfcbfa8e484caba249001bc4449a114
ecpavan <41992735+ecpavan@users.noreply.github.com>
2025-06-06 19:11:03 +0530

    Update Reverse_String.py

--- main/src/python_codes/Reverse_String.py
+++ main/src/python_codes/Reverse_String.py
@@ -1,7 +1,7 @@
 #PYBITES 1 : REVERSE THE STRING
 #"hello" → "olleh"
-# Input String can be given using input command or statuc variable. 
-#Input_string = input("enter the word") 
+# The input string can be provided either through command-line input or as a predefined static variable.
+#input_string = input("enter the word : ") 
 
 input_string = "hello"
 ouptut_string = input_string[::-1]
